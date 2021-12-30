c7e63be695b46bb7d110cf016de031a3b5f62920
Michael <michael.khaikin@gmail.com>
2021-12-30 12:24:32 -0500

    Add: CSS to project component

--- src/components/Project/projectElems.js
+++ src/components/Project/projectElems.js
@@ -1,3 +1,38 @@
 import styled from "styled-components";
 
-export const InfoContainer = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const InfoContainer = styled.div`.
+  display:flex;
+  flex-direction:column;
+  /* max-width: 600px; */
+  align-items:center;
+`;
+export const InfoSection = styled.div`
+  padding: 32px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+export const Title = styled.h4`
+  font-size: 2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
+`;
+export const Description = styled.div``;
+export const Links = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  max-width: 300px;
+  align-items: center;
+`;
+export const GitHub = styled.div``;
+export const Site = styled.div``;
