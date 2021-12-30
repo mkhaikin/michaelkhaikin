94ac140ed36a6dfe1663a48369b19f0da2697550
Michael <michael.khaikin@gmail.com>
2021-12-30 12:24:04 -0500

    Update: html structure

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { InfoContainer } from "./projectElems";
+import {
+  Wrapper,
+  InfoContainer,
+  InfoSection,
+  Title,
+  Description,
+  Links,
+  GitHub,
+  Site,
+} from "./projectElems";
 
 const Project = ({ items }) => {
   let { userId } = useParams();
   let id = userId - 1;
   return (
-    <section className="section">
-      <article className="menu-item">
-        <img src={items[id].img} alt={items[id].title} className="photo" />
-        <div className="item-info">
-          <header>
-            <h4>{items[id].title}</h4>
-            <h4>{items[id].gitHub}</h4>
-          </header>
+    <Wrapper className="wrapper">
+      <InfoContainer className="InfoContainer">
+        <InfoSection className="infoSection">
+          <Title>{items[id].title}</Title>
           <p className="item-text">{items[id].desc}</p>
-        </div>
-      </article>
-    </section>
+        </InfoSection>
+        <Description>
+          <img src={items[id].img} alt={items[id].title} className="photo" />
+          <Links>
+            <GitHub>Repo</GitHub>
+            <Site>Site</Site>
+          </Links>
+        </Description>
+      </InfoContainer>
+    </Wrapper>
   );
 };
 
